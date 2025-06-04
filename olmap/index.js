cf1bbee6cab892d842ae9eeb95c99fce7ebc3c99
SMcE develop <stephenmce@gmail.com>
2025-06-04 21:12:29 +0100

    controls

--- olmap/index.js
+++ olmap/index.js
@@ -4,16 +4,37 @@ import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
 
+import {fromLonLat} from 'ol/proj.js';
+import Graticule from 'ol/layer/Graticule.js';
+import Stroke from 'ol/style/Stroke.js';
+
+//import {defaults as defaultControls} from 'ol/control/defaults.js';
+
+const attributions =
+  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
       source: new OSM(),
+      attributions: attributions,
+    }),
+    new Graticule({
+      // the style to use for the lines, optional.
+      strokeStyle: new Stroke({
+        color: 'rgba(255,120,0,0.9)',
+        width: 2,
+        lineDash: [0.5, 4],
+      }),
+      showLabels: true,
+      wrapX: false,
     }),
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2,
+    center: fromLonLat([4.8, 47.75]),
+    zoom: 3,
   }),
 });
 
