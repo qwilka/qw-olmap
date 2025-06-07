3108321b6a35d8b25c90bc3f0ca4c81056fed6b0
SMcE develop <stephenmce@gmail.com>
2025-06-07 15:16:52 +0100

    config added

---
+++ olmap/gis.js
@@ -0,0 +1,48 @@
+
+
+import './style.css';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+
+import {fromLonLat} from 'ol/proj.js';
+import Graticule from 'ol/layer/Graticule.js';
+import Stroke from 'ol/style/Stroke.js';
+
+//import {defaults as defaultControls} from 'ol/control/defaults.js';
+
+const attributions =
+  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+
+
+export const makeMap = (confData) => {
+    // Create a new map instance
+    const map = new Map({
+        target: 'map',
+        layers: [
+        new TileLayer({
+            source: new OSM(),
+            attributions: attributions,
+        }),
+        new Graticule({
+            // the style to use for the lines, optional.
+            strokeStyle: new Stroke({
+            color: 'rgba(255,120,0,0.9)',
+            width: 2,
+            lineDash: [0.5, 4],
+            }),
+            showLabels: true,
+            wrapX: false,
+        }),
+        ],
+        view: new View({
+        center: fromLonLat([4.8, 47.75]),
+        zoom: 3,
+        }),
+    });
+
+    return true;  
+
+}
